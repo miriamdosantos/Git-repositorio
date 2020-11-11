b2a0320b035b33c2bce8142a8dd19ce582f231b8
miriamdosantos <mii.santos@yahoo.com.br>
2020-11-11 16:35:33 +0000

    upoload arquivo exercises

---
+++ Exercises Fermin/src/exercises/fermin/ExercisesFermin.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercises.fermin;
+
+/**
+ *
+ * @author miriam
+ */
+public class ExercisesFermin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { // start of main method
+       
+    System.out.println(methodGetTotal(23,30));
+    
+       
+    }
+     public static int methodGetTotal (int num1, int num2) {
+       
+         int  sum = num1 + num2;  
+                 
+     return sum;
+        
+    }
+ 
+        // TODO code application logic here
+    }
+    
+
